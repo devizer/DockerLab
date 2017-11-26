7f90e7bac15739dbabb3d83707bf42b9cbd6640b
Volodymyr Hoida <devizer@users.github.com>
2017-11-26 11:55:13 +0300

    Improved fail report

---
+++ TheApp/Extentions/ExceptionExtentions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MongoDB.Profiler
+{
+    public static class ExceptionExtentions
+    {
+        public static string GetExeptionDigest(this Exception ex)
+        {
+            List<string> ret = new List<string>();
+            while (ex != null)
+            {
+                ret.Add("[" + ex.GetType().Name + "] " + ex.Message);
+                ex = ex.InnerException;
+            }
+
+            return string.Join(" --> ", ret);
+        }
+
+    }
+}
