95f0325dc3363d568e0a3c5b080789a31a56a15f
Volodymyr Hoida <devizer@users.github.com>
2017-11-27 03:51:57 +0300

    updated

---
+++ TheApp/ConnectionStringBuilder.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheApp
+{
+    public class ConnectionStringParser
+    {
+        public readonly string ConnectionString;
+
+        public ConnectionStringParser(string connectionString)
+        {
+            ConnectionString = connectionString;
+        }
+
+
+    }
+}
