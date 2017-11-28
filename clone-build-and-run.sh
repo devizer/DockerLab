a8ae120e9dc55e9f534df1b253cc2ce1ff11197d
Volodymyr Hoida <devizer@users.github.com>
2017-11-28 03:53:12 +0300

    cleanup

--- clone-build-and-run.sh
+++ clone-build-and-run.sh
@@ -6,24 +6,10 @@ cd DockerLab/TheApp
 dotnet publish -v:m -c Debug -r linux-x64 -o bin/linux TheApp.sln
 cd bin/linux
 
-function ignore_me_1 {
-./WaitFor \
- "-MySQL=      Server = 192.168.0.8; Port=3306; Uid = mysql; Pwd = mysql; " \
- '-MSSQL=      Data Source = (local)\SQL2005; Integrated Security=true; Timeout = 5;' \
- "-PostgreSQL= Host = 192.168.0.8; Port=5432; User ID=postgres; Password=postgres; Database=postgres;" \
- "-MongoDB=    mongodb://192.168.0.8:27017" \
- "-RabbitMQ=   amqp://192.168.0.8:5672" \
- "-Redis=      192.168.0.8:6379"
-}
-
 cp ../../../container/* .
 docker rmi -f vlad/theapp
 docker rm -f $(docker ps -a -q)
 docker build -t vlad/theapp -f ./Dockerfile .
 
-# docker run -t --rm vlad/theapp ./TheApp --help
-# docker run -t --rm vlad/theapp ./TheApp
-
 export COMPOSE_HTTP_TIMEOUT=61
-# docker-compose -f stack.yml down
 docker-compose -f stack.yml up | tee compose-up.log
