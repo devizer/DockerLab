01b7313aa9b83075dd21eb85ce285f6b5e7e7589
Volodymyr Hoida <devizer@users.github.com>
2017-11-30 08:23:22 +0300

    test security

--- clone-build-and-compose-up.sh
+++ clone-build-and-compose-up.sh
@@ -15,6 +15,6 @@ docker build -t lab/theapp .
 export COMPOSE_HTTP_TIMEOUT=121
 export COMPOSE_PROJECT_NAME=lab
 docker-compose rm -f theapp
+# echo "Kill all the services"; docker-compose -p lab kill; docker-compose -p lab rm -f
 docker-compose up | tee compose-up.log
 
-# docker-compose -p lab kill; docker-compose -p lab rm -f
