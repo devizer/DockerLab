822eeea155afbf199c0841ed492b1185fb78a156
Volodymyr Hoida <devizer@users.github.com>
2017-12-01 04:14:50 +0300

    Solution refactoring

--- build-and-compose-up.sh
+++ build-and-compose-up.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 dotnet publish -v:m -c Debug -r linux-x64 -o bin/linux DockerLab.sln
 
-printf "\n ------------- BUILD WaitFor as lab/theapp -------------"
+printf "\n ------------- BUILD WaitFor as lab/theapp -------------\n"
 pushd WaitFor/bin/linux
 ./WaitFor -Timeout=3 -HttpGet=https://google.com/404
 cp ../../../containers/WaitFor/* .
@@ -10,7 +10,7 @@ docker build -t lab/theapp .
 docker run -it lab/theapp -Timeout=3 -Ping=google.com
 popd
 
-printf "\n ------------- BUILD containers -------------"
+printf "\n ------------- BUILD containers -------------\n"
 cd containers
 export COMPOSE_HTTP_TIMEOUT=121
 export COMPOSE_PROJECT_NAME=lab
