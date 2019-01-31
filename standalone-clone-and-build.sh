d380b2c9b8ea97d8cfc87396796e4874d3b76b76
devizer <devizer@users.github.com>
2019-01-31 20:01:10 +0200

    Updating runtime to 2.1

--- standalone-clone-and-build.sh
+++ standalone-clone-and-build.sh
@@ -3,6 +3,10 @@ work=$HOME/build
 mkdir -p $work; cd $work; rm -rf $work/DockerLab;
 git clone https://github.com/devizer/DockerLab; cd DockerLab
 git pull
-if [[ $(uname -m) == armv7* ]]; then rid=linux-arm; else rid=linux-arm64; fi; echo ".NET Core runtime identifier: $arch"
-dotnet publish -v:m -c Debug -r $rid -o out --self-contained DockerLab.sln
+if [[ $(uname -m) == armv7* ]]; then rid=linux-arm; else rid=linux-arm64; fi; if [[ "$(uname -m)" == "x86_64" ]]; then rid=linux-x64; fi; echo ".NET Core runtime identifier: $rid"
+dotnet publish -v:m -c Debug -r $rid -o out --self-contained true DockerLab.sln
+export Wait_For_Ping=google.com
+export Wait_For_Ping_facebook=facebook.com
+WaitFor/out/WaitFor -Timeout=3 -HttpGet=https://google.com -HttpGet=https://facebook.com -Ping=localhost
+
 
