add1f7284227f8b5ced1df380e2fb30cf6b1e46f
devizer <devizer@users.github.com>
2019-01-31 16:59:38 +0200

    Updating runtime to 2.2

--- standalone-clone-and-build.sh
+++ standalone-clone-and-build.sh
@@ -3,4 +3,5 @@ work=$HOME/build
 mkdir -p $work; cd $work; rm -rf $work/DockerLab;
 git clone https://github.com/devizer/DockerLab; cd DockerLab
 git pull
-sudo bash build-and-compose-up.sh
+if [[ $(uname -m) == armv7* ]]; then rid=linux-arm; else rid=linux-arm64; fi; echo ".NET Core runtime identifier: $arch"
+dotnet publish -v:m -c Debug -r $rid -o out --self-contained DockerLab.sln
