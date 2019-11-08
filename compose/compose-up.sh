4b8193fd84da92c9f431732114eafac91a9dcde1
devizer <devizer@github.io>
2019-11-08 15:42:32 +0200

    new composer fix

--- compose/compose-up.sh
+++ compose/compose-up.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
+docker ps -a | grep local-waitfor | awk '{print $1}' | xargs docker rm -f
+docker images --format "{{.ID}} {{.Repository}}" -q -a | grep local-waitfor | awk '{print $1}' | xargs docker rmi -f
+
 bash v2-build.sh
-docker-compose up --exit-code-from theapp
+docker-compose up --build --exit-code-from theapp
