04632d41225f33bab386d1386ca09337fbca427c
devizer <devizer@github.io>
2019-11-09 03:02:25 +0200

    makeup for compose-up.sh

--- compose/compose-up.sh
+++ compose/compose-up.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
-docker ps -a | grep local-waitfor | awk '{print $1}' | xargs docker rm -f || true
-docker images --format "{{.ID}} {{.Repository}}" -q -a | grep local-waitfor | awk '{print $1}' | xargs docker rmi -f || true
+# docker ps -a | grep local-waitfor | awk '{print $1}' | xargs docker rm -f || true
+# docker images --format "{{.ID}} {{.Repository}}" -q -a | grep local-waitfor | awk '{print $1}' | xargs docker rmi -f || true
 
 bash -e v2-build.sh
-docker-compose up --build --exit-code-from theapp
+docker-compose up --build --force-recreate --exit-code-from theapp
