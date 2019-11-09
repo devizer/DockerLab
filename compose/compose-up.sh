5e83c97eb446e69e716501536cd6a92ea5d397fd
devizer <devizer@github.io>
2019-11-09 05:29:16 +0200

    makeup for ./compose

--- compose/compose-up.sh
+++ compose/compose-up.sh
@@ -3,6 +3,6 @@ set -e
 # docker ps -a | grep local-waitfor | awk '{print $1}' | xargs docker rm -f || true
 # docker images --format "{{.ID}} {{.Repository}}" -q -a | grep local-waitfor | awk '{print $1}' | xargs docker rmi -f || true
 
-bash -e v2-build.sh
+bash -e build.sh
 time docker-compose up --no-start --build --force-recreate 
 time docker-compose up --exit-code-from theapp
